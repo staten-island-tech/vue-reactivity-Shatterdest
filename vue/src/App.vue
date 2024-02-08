13a8e09bf757dcfafdc839339bc0118c91990ced
yi <73318855+Shatterdest@users.noreply.github.com>
2024-02-07 21:53:01 -0500

    hello world!

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,7 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>hello world</div>
+  <HelloWorld />
 </template>
 
 <style scoped>
--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
- 
+    <div><h1>hello world!</h1></div>
 </template>
 
 <style scoped>
